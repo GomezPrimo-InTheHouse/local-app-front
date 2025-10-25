29eaf167a68bc5a8329a4bc1f2d89e38a7ba5c64
GomezPrimo-InTheHouse <juliangomezprimo@gmail.com>
2025-10-25 16:10:03 -0300

    mkdir vercel archives

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// Configuración principal de Vite
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",                // Carpeta de salida
+    chunkSizeWarningLimit: 1200,   // Aumenta el límite de tamaño para evitar warnings
+  },
+});
